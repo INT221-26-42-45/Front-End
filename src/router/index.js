68f5d6d4984b8d4c4855992d31bab0f9ce19ca8f
Niraphon22 <niraphon.ple@mail.kmutt.ac.th>
2021-04-27 16:51:49 +0700

    [UPDATE] initail commit with route path and config tailwind css

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+
+  {
+    path: '/product',
+    name: 'Product',
+    component: () => import('../views/Product.vue')
+  },
+
+  {
+    path: '/contact-us',
+    name: 'ContactUs',
+    component: () => import('../views/ContactUs.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
